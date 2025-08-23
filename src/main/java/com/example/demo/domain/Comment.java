bb69cef5de0b694a11ea15326d4a3adbeb1926d2
DU <sangdyjjang@naver.com>
2025-08-23 14:36:31 +0900

    Feat: Add Entity

--- src/main/java/com/example/demo/domain/Comment.java
+++ src/main/java/com/example/demo/domain/Comment.java
@@ -1,4 +1,33 @@
 package com.example.demo.domain;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Comment {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(nullable = false)
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    //연관 관계
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="post_id")
+    private Post post;
 }
