f100621c20637e5242d29fe31542282764505ad9
DU <sangdyjjang@naver.com>
2025-08-25 13:14:18 +0900

    Feat: User checking func

---
+++ src/main/java/com/example/demo/dto/UserResponseDto.java
@@ -0,0 +1,19 @@
+package com.example.demo.dto;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+
+public class UserResponseDto {
+    private Long id;
+    private String username;
+    private String email;
+    private LocalDateTime createdAt;
+}
