8b46b0ad25d1fb028099445eb757701931f9cc7e
DU <sangdyjjang@naver.com>
2025-08-29 15:34:43 +0900

    Refactor : Dto

--- src/main/java/com/example/demo/dto/PostRequestDto.java
+++ src/main/java/com/example/demo/dto/PostRequestDto.java
@@ -1,13 +1,18 @@
 package com.example.demo.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 //게시글 등록 및 수정
 public class PostRequestDto {
     private String title;
     private String content;
+    private Long userId;
 }
