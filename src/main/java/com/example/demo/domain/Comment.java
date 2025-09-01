fcd21a9dd0ca21de8a49c5dddcecf1d01b502421
DU <sangdyjjang@naver.com>
2025-09-01 10:01:49 +0900

    Feat : Add LDT annotation for auto typed date

--- src/main/java/com/example/demo/domain/Comment.java
+++ src/main/java/com/example/demo/domain/Comment.java
@@ -1,28 +1,37 @@
 package com.example.demo.domain;
 
-
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener; // <-- 이 클래스를 임포트하세요.
 
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name="comments")
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@EntityListeners(AuditingEntityListener.class)
 public class Comment {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
     @Column(nullable = false)
     private String content;
 
+    @CreatedDate
+    @Column(nullable=false, updatable = false)
     private LocalDateTime createdAt;
 
-    //연관 관계
+    @LastModifiedDate
+    @Column(nullable=false)
+    private LocalDateTime updatedAt;
+
+    // 연관 관계
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="user_id")
     private User user;
